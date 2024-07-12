6d45699ed8f6f04a254d9d543dd76f7e5a72774e
Savii <Savindi20>
2024-07-12 14:27:38 +0530

    index file rout dashboard

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,15 +1,20 @@
-import { Text, View } from "react-native";
+import { StatusBar } from "expo-status-bar";
+import {Link } from "expo-router";
+import { Text, View, StyleSheet } from "react-native";
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
+    <View style={styles.container}>
       <Text>Edit app/index.tsx to edit this screen.</Text>
+      <Link href="/dashboard">Dashboard</Link>
+      <StatusBar style="auto" />
     </View>
   );
 }
+ const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  }
+});

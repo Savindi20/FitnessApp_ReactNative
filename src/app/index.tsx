416b5781095358eac4cc7248569dce37d5531b09
Savii <Savindi20>
2024-07-14 12:46:01 +0530

    project updated with food item structure

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,20 +1,31 @@
-import { StatusBar } from "expo-status-bar";
-import {Link } from "expo-router";
 import { Text, View, StyleSheet } from "react-native";
+import { AntDesign } from '@expo/vector-icons';
+
+const FoodListItems = () => {
+  return (
+    <View style={{backgroundColor:'gainsboro', padding:10, borderRadius:5, flexDirection:'row', justifyContent:'space-between', alignItems:'center' }}>
+        <View style={{flex:1, gap:5,}}>
+          <Text style={{fontWeight:'bold', fontSize:16}}>Pizza</Text>
+          <Text style={{color:'dimgray'}}>350 cal, Dominos</Text>
+        </View>
+        <AntDesign name="pluscircleo" size={24} color="royalblue" />
+      </View>
+  )
+}
 
 export default function Index() {
   return (
     <View style={styles.container}>
-      <Text>Edit app/index.tsx to edit this screen.</Text>
-      <Link href="/dashboard">Dashboard</Link>
-      <StatusBar style="auto" />
+      {/* Food Items view */}
+      <FoodListItems />
     </View>
   );
 }
  const styles = StyleSheet.create({
   container: {
     flex: 1,
+    backgroundColor: "#fff",
     justifyContent: "center",
-    alignItems: "center",
+    padding: 10,
   }
 });

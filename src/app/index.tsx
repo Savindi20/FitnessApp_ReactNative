aabe8bc630a280dc6f864a86a4c90169a6af5dad
Savii <Savindi20>
2024-07-15 13:35:20 +0530

    FoodItemList Component added

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,31 +1,27 @@
-import { Text, View, StyleSheet } from "react-native";
-import { AntDesign } from '@expo/vector-icons';
+import { StyleSheet, Text, View, FlatList } from 'react-native';
+import FoodListItem from '../components/FoodListItem';
 
-const FoodListItems = () => {
-  return (
-    <View style={{backgroundColor:'gainsboro', padding:10, borderRadius:5, flexDirection:'row', justifyContent:'space-between', alignItems:'center' }}>
-        <View style={{flex:1, gap:5,}}>
-          <Text style={{fontWeight:'bold', fontSize:16}}>Pizza</Text>
-          <Text style={{color:'dimgray'}}>350 cal, Dominos</Text>
-        </View>
-        <AntDesign name="pluscircleo" size={24} color="royalblue" />
-      </View>
-  )
-}
+const foodItems = [
+  { label: 'Pizza', cal: 75, brand: 'Dominos' },
+  { label: 'Apple', cal: 50, brand: 'Generic' },
+  { label: 'Cooffe', cal: 60, brand: 'Americano' }
+];
 
-export default function Index() {
+export default function App() {
   return (
     <View style={styles.container}>
-      {/* Food Items view */}
-      <FoodListItems />
+      {/* Food Item View */}
+      <FoodListItem item={{ label: 'Pizza', cal: 75, brand: 'Dominos' }} />
+      <FoodListItem item={{ label: 'Apple', cal: 50, brand: 'Generic' }} />
     </View>
   );
 }
- const styles = StyleSheet.create({
+const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
-    justifyContent: "center",
+    backgroundColor: '#fff',
+    justifyContent: 'center',
     padding: 10,
-  }
+    gap: 5,
+  },
 });
